fe7e7ecc4c1f89450431f0cc0d5b679bedfbeec4
tttffff <tristanfellows@icloud.com>
2023-03-17 21:14:51 +0000

    Lint
    And fix one spec

--- spec/01-getting-started/sandwich_spec.rb
+++ spec/01-getting-started/sandwich_spec.rb
@@ -1,15 +1,15 @@
 Sandwich = Struct.new(:taste, :toppings)
 
 RSpec.describe "An ideal sandwich" do
-  let(:sandwich) { Sandwich.new('delicious', []) }
+  let(:sandwich) { Sandwich.new("delicious", []) }
 
   it "is delicious" do
     taste = sandwich.taste
-    expect(taste).to eq('delicious')
+    expect(taste).to eq("delicious")
   end
 
   it "lets me add toppings" do
-    sandwich.toppings << 'cheese'
+    sandwich.toppings << "cheese"
     toppings = sandwich.toppings
     expect(toppings).not_to be_empty
   end
